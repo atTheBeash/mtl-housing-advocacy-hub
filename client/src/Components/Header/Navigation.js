f428d60641b272d33e3d43ad6780e08989f95fe0
Bachar Kanaan <beashohman@gmail.com>
2022-10-25 04:01:36 -0400

    Beash Kanaan - Final Project - Done - Technicality

--- client/src/Components/Header/Navigation.js
+++ client/src/Components/Header/Navigation.js
@@ -6,9 +6,10 @@ const Navigation = () => {
     <>
       <Wrapper>
         <NavigationLink to="/apartment_search">Apartment Search</NavigationLink>
-        <NavigationLink to="/lease_signing">Lease Signing</NavigationLink>
-        <NavigationLink to="/assessment_tool">Assessment Tool</NavigationLink>
-        <NavigationLink to="/moving_out">Moving Out</NavigationLink>
+        <NavigationLink to="/lease_signing">Lease Signing & Moving In</NavigationLink>
+        <NavigationLink to="/assessment_tool">Home Issues (Assessment Tool)</NavigationLink>
+        <NavigationLink to="/moving_out">Evictions, Repossessions, Moving Out</NavigationLink>
+        <NavigationLink to="/post_move">Post Move</NavigationLink>
       </Wrapper>
     </>
   );
